abcd49b31b98d887493e6f48b8b25709d75c399c
Your Name <you@example.com>
2023-12-13 14:02:38 -0300

    Atualização

---
+++ Loja CompreBem/ver-produto.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loja CompreBem</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    
+    <!--INICIO BANNER-->
+    <div class="novoMenu">
+
+        <!--INICIO CONTAINER-->
+        <div class="container">
+
+            <!--INICIO NAVEGAÇÃO TOPO-->
+            <div class="navbar">
+
+                <div class="logo">
+                    <img src="assets/img/logo compre.png" alt="Loja CompreBem" width="125px">
+                </div>
+
+                <!--INICIO MENU NAVEGAÇÃO-->
+                <nav>
+                    <ul id="MenuItens">
+                        <li><a href="index.php" title="">Inicio</a></li>
+                        <li><a href="produtos.php" title="">Produtos</a></li>
+                        <li><a href="" title="">Empresa</a></li>
+                        <li><a href="" title="">Contatos</a></li>
+                        <li><a href="login.php" title="">Minha Conta</a></li>
+                    </ul>
+                </nav>
+                <!--FIM MENU NAVEGAÇÃO-->
+
+                <img src="assets/img/260.png" alt="" width="30px" height="30px">
+            
+                <img src="assets/img/menu.png" alt="" class="menu-celular" onclick="menucelular()">
+
+            </div>
+            <!--FIM NAVEGAÇÃO TOPO-->
+        </div>
+        <!--FIM CONTAINER-->
+
+        <!--INICIO PRODUTOS DETALHES-->
+        <div class="corpo-categorias ver-produto">
+            <div class="linha">
+
+                <div class="col-2"> 
+                    <img src="assets/img/fla1.avif" alt="Camisa Flamengo" id="produtoImg">
+
+                    <!--LINHA DA GALERIA-->
+                    <div class="img-linha">
+                    
+                        <!--INICIO ITEM GALERIA-->
+                     <div class="img-col">
+                        <img src="assets/img/fla0.avif" alt="" width="100%" class="produtoMiniatura">
+                    </div>
+                    <!--FIM ITEM GALERIA-->
+
+                    <!--INICIO ITEM GALERIA-->
+                     <div class="img-col">
+                        <img src="assets/img/fla2.avif" alt="" width="100%" class="produtoMiniatura">
+                    </div>
+                    <!--FIM ITEM GALERIA-->
+
+                    <!--INICIO ITEM GALERIA-->
+                    <div class="img-col">
+                        <img src="assets/img/fla3.avif" alt="" width="100%" class="produtoMiniatura">
+                    </div>
+                    <!--FIM ITEM GALERIA-->
+
+                    <!--INICIO ITEM GALERIA-->
+                    <div class="img-col">
+                        <img src="assets/img/fla1.avif" alt="" width="100%" class="produtoMiniatura">
+                    </div>
+                    <!--FIM ITEM GALERIA-->
+
+                </div>
+                <!--FIM LINHA DA GALERIA-->
+
+                </div>
+
+                <div class="col-2">
+                    <P>Camisa do Flamengo II 23 Masculina adidas</P>
+                    <h1>Compre com desconto</h1>
+                    <h4>R$ 258,99</h4>
+                    <form action="" method="post">
+                        <select name="" id="">
+                            <option value="">Selecione o tamanho</option>
+                            <option value="">P</option>
+                            <option value="">M</option>
+                            <option value="">G</option>
+                            <option value="">XG</option>
+                            <option value="">XXG</option>
+                        </select>
+
+                        <input type="number" name="" id="" value="1">
+
+                        <button type="submit" class="btn">Adicionar ao carrinho</button>
+                    </form>
+            
+                    <h3>Descrição:</h3>
+                    <p>	AEROREADY: Desenvolvida para proporcionar respirabilidade e absorção do suor para comodidade duradoura.</p>
+                
+                </div> 
+            </div>
+        </div>
+        <!--FIM PRODUTOS DETALHES-->
+
+        <!--INICIO TITULO PRODUTOS DETALHES-->
+        
+        <!--FIM  TITULO PRODUTOS DETALHES-->
+
+        <div class="corpo-categorias">
+        <div class="linha linha2">
+            <h2>Produtos Relacionados</h2>
+            <p>Veja Mais</p>
+        </div>
+    </div>
+     <!--FIM BANNER-->
+
+     <!--INICIO PRODUTOS EM DESTAQUE-->
+        
+         
+        <!--INICIO LINHA DO CORPO CATEGORIAS EM DESTAQUE-->
+        <div class="linha">
+
+            <!--INICIO ITEM PRODUTOS EM DESTAQUE-->
+            <div class="col-4">
+                <a href="ver-produto.php" title="">
+                <img src="assets/img/fla1.avif" alt="">
+                </a>
+                <h4>Camisa Adidas Flamengo I 2023 com Patrocínio</h4>
+                <div class="classificacao">
+                    <ion-icon name="star"></ion-icon>
+                    <ion-icon name="star"></ion-icon>
+                    <ion-icon name="star"></ion-icon>
+                    <ion-icon name="star"></ion-icon>
+                    <ion-icon name="star"></ion-icon>
+                </div>
+                <p>R$ 319,90</p>
+            </div>
+            <!--FIM ITEM PRODUTOS EM DESTAQUE-->
+            <!--INICIO ITEM PRODUTOS EM DESTAQUE-->
+            <div class="col-4">
+                <a href="ver-produto2.php" title="">
+                <img src="assets/img/botafogo.webp" alt="">
+                </a>
+                <h4>Camisa Botafogo Retrô 1942 Multicores</h4>
+                <div class="classificacao">
+                    <ion-icon name="star"></ion-icon>
+                    <ion-icon name="star"></ion-icon>
+                    <ion-icon name="star"></ion-icon>
+                    <ion-icon name="star-outline"></ion-icon>
+                    <ion-icon name="star-outline"></ion-icon>
+                </div>
+                <p>R$ 139,90</p>
+            </div>
+            <!--FIM ITEM PRODUTOS EM DESTAQUE-->
+            <!--INICIO ITEM PRODUTOS EM DESTAQUE-->
+            <div class="col-4">
+                <img src="assets/img/vasco.webp" alt="">
+                <h4>Camisa Kappa Vasco II 2023</h4>
+                <div class="classificacao">
+                    <ion-icon name="star"></ion-icon>
+                    <ion-icon name="star"></ion-icon>
+                    <ion-icon name="star"></ion-icon>
+                    <ion-icon name="star"></ion-icon>
+                    <ion-icon name="star-half"></ion-icon>
+                </div>
+                <p>R$ 299,90</p>
+            </div>
+    
+          
+            <!--FIM ITEM PRODUTOS EM DESTAQUE-->
+        </div>
+        <!--FIM LINHA DO CORPO CATEGORIAS EM DESTAQUE-->
+        
+
+     <!--INCIO RODAPÉ-->
+    <footer class="rodape">
+
+        <div class="container">
+            <div class="linha">
+
+                <div class="rodape-col-1">
+                <h3>Baixe o nosso app</h3>
+                <p>Baixe nosso app para Android e IOS</p>
+                <div class="app-logo">
+                    <img src="assets/img/google.png" alt="">
+                    <img src="assets/img/apple.png" alt="">
+                </div>
+                </div>
+
+                <div class="rodape-col-2">
+                    <img src="assets/img/logo compre.png" alt="">
+                    <p>Loja CompreBem © 2023. Todos os direitos reservados.</p>
+                </div>
+
+                <div class="rodape-col-3">
+                    <h3>Mais Informações</h3>
+                    <ul>
+                        <li>Cupons</li>
+                        <li>Blog</li>
+                        <li>Política de Privacidade</li>
+                        <li>Contatos</li>
+                    </ul>
+                </div>
+
+                <div class="rodape-col-4">
+                    <h3>Redes Sociais</h3>
+                    <ul>
+                        <li>Facebook <ion-icon name="logo-facebook"></ion-icon></li>
+                        <li>Twitter <ion-icon name="logo-twitter"></ion-icon></li>
+                        <li>Instagram <ion-icon name="logo-instagram"></ion-icon></li>
+                        <li>Youtube <ion-icon name="logo-youtube"></ion-icon></li>
+                    </ul>
+                    </div>
+
+            </div>
+            <hr>
+            <p class="direitos">
+                &#169; Todos os direitos reservados a Loja CompreBem.</p>
+            </p>    
+        </div>
+
+     </footer>
+     <!--FIM RODAPÉ-->
+
+
+
+
+
+
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+<script src="assets/js/app.js"></script>
+
+</body>
+</html>
