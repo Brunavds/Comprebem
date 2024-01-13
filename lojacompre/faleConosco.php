6a65e1f4ad4b6a0b42719dc75e2d1be0939f2693
Programador de Sistemas - 2023.2 <1696854pj2023.2@senacrjedu.local>
2024-01-12 21:32:17 -0300

    Fale conosco

--- lojacompre/faleConosco.php
+++ lojacompre/faleConosco.php
@@ -8,7 +8,6 @@
     <link rel="icon" href="./Logos/Logotipo Escuro.png">
     <!-- CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<link rel="stylesheet" href="fl.css">
     <!-- jQuery -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <!-- JavaScript -->
@@ -25,22 +24,24 @@
 	<div >
 	
 
-		<h1 class= "text">Fale Conosco</h1> 
+		<h1 class= "text" style= 
+		"text-align: center; margin-top: 50px; font-weight: bold  ;  "> 
+		Fale Conosco</h1> 
 	
 
-		<div class="cont">
+		<div class="cont" style= "text-align: left; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; color: black; margin-top: 30px;">
 		<p>Se você tem duvidas ou suguestões entra em contato conosco através de :</p> 
 		<h4 class = "text1">Email: loja_comprebem@gmail.com</h4>
 		<h4 class = "text1">WhatsApp: (21) 3352-7125 </h4>
 		
 		
-		<p class="p">Siganos também nas redes sociais</p>
+		<p class="p">Nos siga também nas redes sociais</p>
 
-		<h4 class = "text1">Instagran: @Comprebem </h4>
+		<h4 class = "text1">Instagram: @Comprebem </h4>
 		<h4 class = "text1"> Facebook: CompreBem </h4>
 		</div>
 
-		<div>
+		<div style= "box-align: right; margin-left: 50%; width: 600px; margin-bottom: 30px; margin-top: -100px;">
 			<img class="img"src="Produtos/agente.jpg" alt="agente">
 		</div>
 	</div> 
